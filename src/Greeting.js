d7982b2b592ad528e71fd87237fb07906d40fd1f
Salman <muhammadsalman410@gmail.com>
2023-08-22 16:17:26 +0500

    first commit

--- src/Greeting.js
+++ src/Greeting.js
@@ -3,8 +3,36 @@ import "./style.css";
 const Greeting = () => {
   return (
     <>
-      <h1>Hello Greetings</h1>
+      <div className="container">
+        <div className="row">
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+        </div>
+      </div>
     </>
+  );
+};
+
+const Book = () => {
+  return (
+    <div className="col-md-3 my-3">
+      <div className="card">
+        <img
+          src={"https://m.media-amazon.com/images/I/41+iW8nZKAL._AC_UL320_.jpg"}
+          className="card-img-top img-specs"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
